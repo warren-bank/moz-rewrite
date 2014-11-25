f82015f9a0bc389d9917c67279b341c00d535351
Warren R Bank <warren.b.415@gmail.com>
2014-11-25 14:49:09 -0800

    additional default wget command-line option: "--progress=dot:binary"
    notes regarding my own testing of wget.exe binaries under Windows:
    
    * the binaries that I use come from:
        http://opensourcepack.blogspot.com/2010/05/wget-112-for-windows.html
    
    * my initial tests were using wget-1.14:
        http://osspack32.googlecode.com/files/wget-1.14.exe
      observations:
        - the continue "-c" switch to resume broken downloads doesn't work properly
    
    * subsequent tests were using wget-1.16:
        http://sourceforge.net/projects/tumagcc/files/wget-1.16.7z/download
      observations:
        - download continuation "-c" works perfectly
        - the default progress indicator is fairly horrible.
          _this_ commit adds another switch to configure the display of this indicator.

--- defaults/preferences/moz-rewrite.js
+++ defaults/preferences/moz-rewrite.js
@@ -13,4 +13,4 @@ pref("extensions.Moz-Rewrite.request_persistence.save_file.path","");
 pref("extensions.Moz-Rewrite.request_persistence.save_file.maximum_capacity",10);
 pref("extensions.Moz-Rewrite.request_persistence.replay.download_directory.path","{DfltDwnld}");
 pref("extensions.Moz-Rewrite.request_persistence.replay.run.wget.executable_file.path","/usr/bin/wget");
-pref("extensions.Moz-Rewrite.request_persistence.replay.run.wget.options","-c -nd --content-disposition --no-http-keep-alive --no-check-certificate -e robots=off");
+pref("extensions.Moz-Rewrite.request_persistence.replay.run.wget.options","-c -nd --content-disposition --no-http-keep-alive --no-check-certificate -e robots=off --progress=dot:binary");
