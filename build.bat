925930063d57dd717bef24a2f382446cc75d6e4c
Warren R Bank <warren.b.415@gmail.com>
2014-12-16 14:10:17 -0800

    [v1.01] renamed addon to: "Rewrite HTTP Headers"
    * the real addon is named: "moz-rewrite"
      (an obvious play on the name of the popular Apache httpd module)
    * the git repo for this "lite" or "safe" version is: "moz-rewrite-amo"
      because this version is specifically for hosting on AMO
    * the codebase actually refers to itself as: "moz-safe-rewrite"
      ..just because it seemed appropriate at the time
    * however, this addon can't be named "moz-safe-rewrite" on AMO.
      I figured that "Rewrite HTTP Headers" would be a more descriptive name.
      also, more user friendly.
    * this commit is simply a "rebranding"

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 rem :: http://sevenzip.sourceforge.jp/chm/cmdline/commands/add.htm
 
-7z a -tzip -scsUTF-8 "moz-safe-rewrite.xpi" ".\chrome.manifest" ".\install.rdf" ".\LICENSE" ".\README.md" "chrome\" "components\04_modular_addon\" "defaults\"
+7z a -tzip -scsUTF-8 "rewrite-http-headers.xpi" ".\chrome.manifest" ".\install.rdf" ".\LICENSE" ".\README.md" "chrome\" "components\04_modular_addon\" "defaults\"
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-zip -r moz-safe-rewrite.xpi chrome.manifest install.rdf LICENSE README.md chrome components defaults
+zip -r rewrite-http-headers.xpi chrome.manifest install.rdf LICENSE README.md chrome components defaults
